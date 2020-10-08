17d9334dd9a210f1166afab83d2029d05b14da2a
PatrickJReed <preed@salk.edu>
2020-10-08 10:24:56 -0700

    all code used in repair seq paper

--- makeBedGraph.sh
+++ makeBedGraph.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 folder=$1
 path="/raidixshare_log-g/Dylan/190617_PE75/RepairSeq_062819/work/align/"
-/home/preed/bin/makeTagDirectory $folder $path$folder"/"$folder"-sort.bam" -single
+/home/preed/bin/makeTagDirectory $folder $path$folder"/"$folder"-ready.bam" -single
 /home/preed/bin/makeUCSCfile $folder -o auto
